e8fa6b154f745b2005ca2321272c34168f6309f7
Luca Stefanutti <59293559+UnderDogCoder911@users.noreply.github.com>
2022-01-06 11:36:37 -0500

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,32 @@
+# 2022Regaining-skills
+#Luca
+#Stefanutti
+#Pasta is my weakness
+
+#defining inital letter L
+L = """L
+L
+L
+L
+L
+L
+LLLLL
+    """
+#printing out L
+#print(L)
+
+#defining last initial S
+S = """ 
+ SSS 
+S   S
+S
+ SSS
+    S
+S   S
+ SSS      
+"""
+#printing S
+#print(S)
+
+#concatenation
+print (L + S)
